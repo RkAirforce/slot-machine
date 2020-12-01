1967d6abab8235e771fb43eb22e07b30c03af841
EC2 Default User <ec2-user@ip-172-31-25-74.ap-northeast-1.compute.internal>
2020-12-01 04:26:17 +0000

    first commit

---
+++ lesson_memo_app.rb
@@ -0,0 +1,19 @@
+require "csv"
+
+puts "1(新規でメモを作成) 2(既存のメモ編集する)"
+
+memo_type = gets.to_s.chomp
+
+# 続きを書いていきましょう！！(ifで条件分岐)
+if memo_type == "1"
+  puts "拡張子を除いたファイルを入力してください"
+  file_name = gets.to_s.chomp
+  puts "メモしたい内容を記入してください"
+  puts "完了したらCtrl + Dを　おします"
+  content = readlines
+  CSV.open("#{file_name}.csv", "w") do |csv|
+    csv << content
+  end
+elsif memo_type == '2'
+  puts "編集したい拡張子を除いたファイルを入力してください"
+end
